e1d8b2b945c0764f96689eaa6205722b3dc13266
Antonio Jesús Espín Mena <Antonio Jess Espn Mena>
2017-03-05 19:21:16 +0100

    AJAX added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-(function () {
-   'use strict';
-var codeElement = document.getElementsByClassName('about');
 
+      require(["dojo/fx", "dojo/on", "dojo/dom", "dojo/domReady!"], function(fx, on, dom) {
 
+          var wipeInButton = dom.byId("wipeInButton");
+          var wipeOutButton = dom.byId("wipeOutButton");
+          var  wipeTarget = dom.byId("wipeTarget");
+          var wipeInButton2 = dom.byId("wipeInButton2");
+          var  wipeOutButton2 = dom.byId("wipeOutButton2");
+          var  wipeTarget2 = dom.byId("wipeTarget2");
 
+                      on(wipeInButton, "click", function(evt){
+                        fx.wipeIn({ node: wipeTarget }).play();
+                      });
+                      on(wipeOutButton, "click", function(evt){
+                        fx.wipeOut({ node: wipeTarget }).play();
+                      });
+                      on(wipeInButton2, "click", function(evt){
+                        fx.wipeIn({ node: wipeTarget2 }).play();
+                      });
+                      on(wipeOutButton2, "click", function(evt){
+                        fx.wipeOut({ node: wipeTarget2 }).play();
+                      });
 
-
-}());
-var codeElement = document.getElementsByClassName('about');
+      });
